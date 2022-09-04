f92a6d06b39b74dbf5062c0ad09552c5065f3263
nikithasrii <98526622+nikithasrii@users.noreply.github.com>
2022-09-04 07:03:50 +0530

    def prime(n):   if n>1:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return(False)         else:             return(True) t=int(input()) for i in range(t):     j=int(input())     for i in range(j):         if prime(j-i):             print(j-i)             break         elif prime(j+i):             print(j+i)             break

---
+++ Nearest_Prime.py
@@ -0,0 +1,17 @@
+def prime(n):  
+  if n>1:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return(False)
+        else:
+            return(True)
+t=int(input())
+for i in range(t):
+    j=int(input())
+    for i in range(j):
+        if prime(j-i):
+            print(j-i)
+            break
+        elif prime(j+i):
+            print(j+i)
+            break
